8936bfe61612ee1dfb98202d7cf7adc26b2341b4
RtjShreyD <40147386+RtjShreyD@users.noreply.github.com>
2018-07-01 14:26:32 +0530

    Delete service.py

--- service.py
+++
@@ -1,8 +0,0 @@
-import json
-with open('example_1.json','r') as input:
-	obj = json.load(input)
-	with open('output.txt', 'w') as output:
-		output.write(obj['name'] + "'s Taste:\n")
-		for tasty in obj['Taste']:
-			output.write(tasty + "\n")
-			
